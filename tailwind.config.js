7e8135ef70e79e96080e8919ab3b5eabe6cfedb5
Marco Romeny <marco@mimecom.net>
2023-07-18 09:01:14 +0200

    Changed front link labels

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,13 @@ module.exports = {
   ],
   theme: {
     extend: {
+      dropShadow: {
+        'contrast': '0 0 0.1rem rgba(0, 0, 0, 0.15)',
+        '4xl': [
+            '0 35px 35px rgba(0, 0, 0, 0.25)',
+            '0 45px 65px rgba(0, 0, 0, 0.15)'
+        ]
+      },
       colors:{
         'wu-nav':'var(--nav,#2000CD)',
         'wu-text':'var(--text,#FFFFFF)',
